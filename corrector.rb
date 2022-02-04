fecd670cbc03fbee21ec173e0c7744c58577b2f3
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-04 14:01:55 -0500

    Set up decorator with composition

--- corrector.rb
+++ corrector.rb
@@ -1,15 +1,10 @@
-require_relative 'person'
-
 # Define Corrector class
-class Corrector < Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super
+class Corrector
+  def initialize(name)
+    @name = name
   end
 
   def correct_name
     @name.capitalize[0, 9]
   end
 end
-
-test2 = Corrector.new(30, 'luis fernando jimenez')
-test2.correct_name

0f9312b034c467ac247af07d1b4fad4932083897
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-04 13:34:40 -0500

    Setup decorator with inheritance

--- corrector.rb
+++ corrector.rb
@@ -1,6 +1,15 @@
+require_relative 'person'
+
 # Define Corrector class
-class Corrector
-  def correct_name(name)
-    name[0, 9].capitalize
+class Corrector < Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super
+  end
+
+  def correct_name
+    @name.capitalize[0, 9]
   end
 end
+
+test2 = Corrector.new(30, 'luis fernando jimenez')
+test2.correct_name
